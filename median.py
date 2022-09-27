e92007a526f8535c67abd1e77764d3193fafa1a0
Saskia Lablans <k21009051@kcl.ac.uk>
2022-09-27 11:39:31 +0100

    Add content to process median of a given list

--- median.py
+++ median.py
@@ -1,5 +1,4 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 while True:
     try:
@@ -9,4 +8,20 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+
+if len(numbers) == 0:
+    print('Error - empty list')
+    median = None
+
+elif len(numbers) % 2 == 0:
+    lower_value = numbers[(len(numbers)//2)-1]
+    upper_value = numbers[len(numbers)//2]
+    
+    median = (lower_value + upper_value) / 2
+
+elif len(numbers) % 2 == 1:
+    median = numbers[len(numbers)//2]
+
+print(median)
